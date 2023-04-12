4e179df1f1ba9942486fb697e731023fa596fd11
nelf <nelf@nelf.com>
2023-04-12 17:26:37 +0700

    test-1

--- backend/src/main/java/ru/nelf/backend/controller/Controller.java
+++ backend/src/main/java/ru/nelf/backend/controller/Controller.java
@@ -8,37 +8,42 @@ import ru.nelf.backend.entity.ResponseCategoryFiles;
 import ru.nelf.backend.service.StorageService;
 
 @RestController
-@CrossOrigin
 public class Controller {
 
     @Autowired
     private StorageService storageService;
 
+    @CrossOrigin
     @GetMapping(value = "/all")
     public ResponseCategoryFiles getAllImages() {
         return storageService.getAll();
     }
 
+    @CrossOrigin
     @GetMapping(value = "/test")
     public String test(){
         return "test";
     }
 
+    @CrossOrigin
     @GetMapping(value = "/{filename}")
     public Response getImage(@PathVariable String filename) {
         return storageService.getImage(filename + ".jpg");
     }
 
+    @CrossOrigin
     @GetMapping(value = "/")
     public ResponseCategoryFiles getCategoryFiles(@RequestParam String category){
         return storageService.findByCategory(category.toLowerCase());
     }
 
+    @CrossOrigin
     @PostMapping(value = "/")
     public Response putImage(@RequestBody Request request) {
         return storageService.putImage(request);
     }
 
+    @CrossOrigin
     @DeleteMapping(value = "/{filename}")
     public Response deleteImage(@PathVariable String filename){
         return storageService.deleteImage(filename + ".jpg");
